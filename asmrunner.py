f83be48d63fde9d6bf259758fc48d332227dd9ca
h4n24wny!n3 <57664151+HanZawNyine@users.noreply.github.com>
2021-01-22 03:06:23 +0630

    Update asmrunner.py

--- asmrunner.py
+++ asmrunner.py
@@ -6,3 +6,5 @@ os.system("sudo mv asm /usr/local/bin/ && sudo chmod +x /usr/local/bin/asm")
 #asm.py
 os.system("sudo mv asm.py /usr/local/bin ")
 os.system("sudo chmod +x /usr/local/bin/asm.py")
+
+print("Assembly Runner added Successfully ! \n  usage : asm (File Name).asm")
