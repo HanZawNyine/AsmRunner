c0245a20a7eb3c52723d96cae45eaad3b79671e1
h4n24wny!n3 <57664151+HanZawNyine@users.noreply.github.com>
2021-01-22 03:00:17 +0630

    Add files via upload

---
+++ asm.py
@@ -0,0 +1,20 @@
+                                 
+import os
+import sys
+
+aa = sys.argv[1]
+print("\033[1;34;40m Input : "+ aa+"\n")
+
+li = list(aa.split("."))
+#print(*li)
+
+FCom = "nasm -f elf32 "+li[0]+".asm -o "+li[0]+".o"
+os.system(FCom)
+#print(FCom)
+
+SCom = "ld -m elf_i386 "+li[0]+".o -o "+li[0]
+os.system(SCom)
+
+TCom = "./"+li[0]  
+os.system(TCom)
+
---
+++ asmrunner.py
@@ -0,0 +1,8 @@
+import os
+
+#asm
+os.system("sudo mv asm /usr/local/bin/ && sudo chmod +x /usr/local/bin/asm")
+
+#asm.py
+os.system("sudo mv asm.py /usr/local/bin ")
+os.system("sudo chmod +x /usr/local/bin/asm.py")
