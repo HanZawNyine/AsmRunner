c3db3b62e265ea342c9977dfcb072200801682bf
h4n24wny!n3 <57664151+HanZawNyine@users.noreply.github.com>
2021-01-22 18:43:05 +0630

    Update asmrunner.py

--- asmrunner.py
+++ asmrunner.py
@@ -2,9 +2,13 @@ import os
 
 #asm
 os.system("sudo mv asm /usr/local/bin/ && sudo chmod +x /usr/local/bin/asm")
+os.system("sudo mv asm64 /usr/local/bin/ && sudo chmod +x /usr/local/bin/asm64")
 
 #asm.py
 os.system("sudo mv asm.py /usr/local/bin ")
 os.system("sudo chmod +x /usr/local/bin/asm.py")
 
-print("Assembly Runner added Successfully ! \n  usage : asm (File Name).asm \n Developed By h4n24wny!n3")
+os.system("sudo mv asm64.py /usr/local/bin ")
+os.system("sudo chmod +x /usr/local/bin/asm64.py")
+
+print("Assembly Runner added Successfully ! \n  usage : asm or asm64 (File Name).asm \n Developed By h4n24wny!n3")
